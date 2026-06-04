1d1301d516781335ab03ce2f58bf07207d879bc1
“2-six” <mideng13@qq.com>
2026-06-04 19:39:42 +0800

    JobLevel初步（前端）

--- vhr-front/src/router/index.js
+++ vhr-front/src/router/index.js
@@ -12,7 +12,7 @@ const routes = [
       { path: '', component: () => import('../views/Welcome.vue') },
        { path: 'department', component: () => import('../views/Department.vue') },
       { path: 'position', component: () => import('../views/Position.vue') },
-      // { path: 'joblevel', component: () => import('../views/JobLevel.vue') },
+       { path: 'joblevel', component: () => import('../views/JobLevel.vue') },
       // { path: 'employee', component: () => import('../views/Employee.vue') }
     ]
   }
---
+++ vhr-front/src/views/JobLevel.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="joblevel">
+    <div class="title">职称管理</div>
+
+    <el-card shadow="hover">
+      <!-- 工具栏 -->
+      <div class="toolbar">
+        <el-button type="primary" @click="add">新增职称</el-button>
+        <el-button type="success" @click="edit">修改职称</el-button>
+        <el-button type="danger" @click="del">删除职称</el-button>
+      </div>
+
+      <!-- 表格 -->
+      <el-table :data="tableData" border stripe>
+        <el-table-column prop="id" label="ID" width="80" />
+        <el-table-column prop="name" label="职称名称" />
+        <el-table-column prop="titleLevel" label="职称等级" />
+        <el-table-column prop="createDate" label="创建时间" width="180" />
+      </el-table>
+    </el-card>
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+import request from '../utils/request'
+import { ElMessage } from 'element-plus'
+
+const tableData = ref([])
+
+// 页面加载
+onMounted(() => {
+  loadList()
+})
+
+// 获取列表
+const loadList = async () => {
+  const res = await request.get('/joblevel/list')
+  tableData.value = res.data
+}
+
+// 新增
+const add = () => {
+  ElMessage.success('打开新增弹窗')
+}
+
+// 修改
+const edit = () => {
+  ElMessage.success('打开修改弹窗')
+}
+
+// 删除
+const del = () => {
+  ElMessage.warning('执行删除')
+}
+</script>
+
+<style scoped>
+.joblevel {
+  padding: 20px;
+}
+.title {
+  font-size: 20px;
+  font-weight: bold;
+  margin-bottom: 20px;
+}
+.toolbar {
+  margin-bottom: 15px;
+}
+</style>
